149b6dfb0f6f37d09b96dcfeedaf6e590a692b2e
UVLAlumni <universityvisionleadership@gmail.com>
2025-06-03 23:41:25 -0700

    Add CORS headers to create-checkout-session API

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -2,6 +2,16 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
+  // Add CORS headers to allow all origins (or customize if you want)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // Handle OPTIONS preflight request
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).send({ error: 'Method Not Allowed' });
   }
